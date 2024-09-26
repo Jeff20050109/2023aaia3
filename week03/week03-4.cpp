cf2cc6788030fe27c2994cdd1577143caa08a6a0
Jeff20050109 <144758510+Jeff20050109@users.noreply.github.com>
2024-09-26 16:30:25 +0800

    week03

---
+++ week03/week03-4.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        //http://cplusplus.com/reference/string/string/find/
+        return haystack.find(needle);
+    }
+};
