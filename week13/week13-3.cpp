449977f08e256d3d91e851c97f8f151fdca294b7
Jeff20050109 <144758510+Jeff20050109@users.noreply.github.com>
2024-12-05 16:34:49 +0800

    week13

---
+++ week13/week13-3.cpp
@@ -0,0 +1,11 @@
+int main() {
+    int a,b;
+    vector<int>A,B;
+    while(cin>>a>>b){
+        A.push_back(a);
+        B.push_back(b);
+    }
+    for(int i=0;i<A.size();i++){
+        cout<<A[i]<<" ";
+    }
+}
---
+++ week13/week13-4.cpp
@@ -0,0 +1,16 @@
+int main() {
+    int a,b;
+    vector<int>A,B;
+    while(cin>>a>>b){
+        A.push_back(a);
+        B.push_back(b);
+    }
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    int ans=0;
+    for(int i=0;i<A.size();i++){
+
+        ans+=abs(A[i]-B[i]);
+    }
+    cout<<" µª®×¬O: "<<ans;
+}
